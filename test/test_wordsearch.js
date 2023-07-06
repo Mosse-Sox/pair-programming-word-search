f6e95f8d05a05ec210469f82ccd1b4cd10f2b5f2
Mosse-Sox <autumngreenie990@gmail.com>
2023-07-06 03:29:39 +0000

    Pair Programmed, added functionality to search for words vertically, handles cases where there is no array or the array doesnt exist

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,7 +4,7 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return false if the word is not present",() => {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", ()  => {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,27 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically",() => {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'B', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'E', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'A', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'N', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'BEAN');
+
+    assert.isTrue(result);
+  });
+
+  it("should return 'No letters' if there is no word matrix", () => {
+    const result = wordSearch([], 'BEAN');
+
+    assert.strictEqual(result, 'No word matrix');
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (!Array.isArray(letters) || letters.length === 0) {
+      return "No word matrix";
     }
-}
-
-module.exports = wordSearch
+  
+    const horizontalJoin = letters.map((ls) => ls.join(""));
+    const verticalTranspose = letters[0].map((_, colIndex) =>
+      letters.map((row) => row[colIndex])
+    );
+    const verticalJoin = verticalTranspose.map((ls) => ls.join(""));
+  
+    for (const l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+  
+    for (const l of horizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+    return false;
+  };
+  
+  module.exports = wordSearch;
+  
